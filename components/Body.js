370c80cbfb8398ad709096127667aa0da7a30602
Josiah2122 <98037818+Josiah2122@users.noreply.github.com>
2023-02-20 12:39:52 +0300

    Update Body.js

--- components/Body.js
+++ components/Body.js
@@ -1,22 +1,68 @@
 import React from "react"
 
+
 export default function Body(){
- function handleClick(){
-  console.log("i was clicked");
+ const [meme, setMeme] = React.useState({
+  topText: "One does not simply",
+  bottomText: "walk into mordor",
+  randomImage: "http://i.imgflip.com/1bij.jpg" 
+})
+const [allMemes, setAllMemes] = React.useState([])
+
+ React.useEffect(() => {
+ fetch("https://api.imgflip.com/get_memes")
+  .then(res => res.json())
+  .then(data => setAllMemes(data.data.memes))
+ }, [])
+
+ // console.log(allMemes)
+ 
+ function getMemeImage() {
+  const randomNumber = Math.floor(Math.random() * allMemes.length)
+  const url = allMemes[randomNumber].url
+   setMeme(prevMeme => ({
+    ...prevMeme,
+    randomImage: url
+    }))
  }
+    
+ function handleChange(event) {
+  const {name, value} = event.target
+   setMeme(prevMeme => ({
+    ...prevMeme,
+    [name]: value
+  }))
+ }
+
  return(
   <div className="container-1">
    <div className="sub-container-1">
     <div className="input-container">
-     <input type="text" className="inputs" placeholder="Top text" />
-     <input type="text" className="inputs" placeholder="Bottom text" />
+
+     <input 
+       type="text"
+       className="inputs input1" 
+       placeholder="Top text"
+       name="topText" 
+       value={meme.topText}
+       onChange={handleChange}
+      />
+     <input 
+      type="text" 
+      className="inputs input2" 
+      placeholder="Bottom text" 
+      name="bottomText"
+      value={meme.bottomText}
+      onChange={handleChange}
+     />
+
     </div>
-    <button onClick={handleClick}>Generate a new meme image 🖼</button>
+    <button onClick={getMemeImage}>Generate a new meme image 🖼</button>
    </div>
    <div className="sub-container-2">
-    <img src="./images/memeimg.png"/>
-    <h3 className="text-1">SHUT UP</h3>
-    <h3 className="text-2">AND TAKE MY MONEY</h3>
+    <img src={meme.randomImage}/>
+    <h3 className="text-1 meme-text">{meme.topText}</h3>
+    <h3 className="text-2 meme-text">{meme.bottomText}</h3>
    </div>
   </div>
  )
